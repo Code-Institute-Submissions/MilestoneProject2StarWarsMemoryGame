5cd740000d9c2ac3c480d3c6c40dcd77b949ad2c
Murphj99 <john.murphy99@mail.dcu.ie>
2020-04-19 11:32:13 +0000

    beautify all html, css and js

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,115 +7,116 @@ var shufflenumber = cards.length; //shuffle number equals the amount of cards in
 
 cards.forEach(card => card.addEventListener('click', flipCard)); //add eventlistener to cards, if its clicked - flip it
 
-$(function() { // shuffle on load screen
-    shuffle();
+$(function () { // shuffle on load screen
+	shuffle();
 });
 
-function flipCard() {  //adds class of flip to card
-  if (lockBoard) return;
-  if (this === firstCard) return;
+function flipCard() { //adds class of flip to card
+	if (lockBoard) return;
+	if (this === firstCard) return;
 
-  this.classList.add('flip');
+	this.classList.add('flip');
 
-  if (!hasFlippedCard) { //first click 
-    hasFlippedCard = true;
-    firstCard = this;
+	if (!hasFlippedCard) { //first click 
+		hasFlippedCard = true;
+		firstCard = this;
 
-    return;
-  }
-  secondCard = this; //second click
+		return;
+	}
+	secondCard = this; //second click
 
-    numberAttempts++; //adds 1 to number of attempts after every two clicks
-    attempts(); //calls function attempts
+	numberAttempts++; //adds 1 to number of attempts after every two clicks
+	attempts(); //calls function attempts
 
 
-  checkForMatch(); // calls function check for match
+	checkForMatch(); // calls function check for match
 }
 
 function attempts() { //adds the updated number of attempts +1 to the html element continously after every second click
-    $('.numberattempts').html(numberAttempts);
+	$('.numberattempts').html(numberAttempts);
 }
 
 function checkForMatch() { //checks if the data-name in first card matches the data-name in the second card
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+	let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
-  isMatch ? disableCards() : unflipCards(); // it's a match call function to disable the cards unflipping - otherwise unflip them
+	isMatch ? disableCards() : unflipCards(); // it's a match call function to disable the cards unflipping - otherwise unflip them
 }
 
 function disableCards() { //if matches removes ability to click and adds match class, calls win function check if player has won
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  firstCard.classList.add('match');
-  secondCard.classList.add('match');
-  win();
-  resetBoard();
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+	firstCard.classList.add('match');
+	secondCard.classList.add('match');
+	win();
+	resetBoard();
 }
 
 function unflipCards() { // if cards do not match it removes the flip classes and the cards flip back
-  lockBoard = true;
+	lockBoard = true;
 
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
 
-    resetBoard();
-  }, 1200);
+		resetBoard();
+	}, 1200);
 }
 
 function resetBoard() { // resets for remaining cards and restarts the process 
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+	[hasFlippedCard, lockBoard] = [false, false];
+	[firstCard, secondCard] = [null, null];
 }
 
 function shuffle() { //shuffles cards based on the shufflenumber and puts them in random order 
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * shufflenumber);
-    card.style.order = randomPos;
-  });
+	cards.forEach(card => {
+		let randomPos = Math.floor(Math.random() * shufflenumber);
+		card.style.order = randomPos;
+	});
 }
 
 
 function restart() { //restarts the game - tied to the restart and try again button, shuffles, removes flip and match classes, sets number attempts to 0, adds event listener, reshows the cards and hides the results modal
-    cards.forEach(card => { // ****Future Bug fix - For some reason when I add the function shuffle() here it stops gameplay but when I duplicate the shuffle code it operates as it should. Will investigate further to avoid code duplication.  
-    let randomPos = Math.floor(Math.random() * shufflenumber );
-    card.style.order = randomPos;  
-    card.classList.remove('flip', 'match'); 
-    numberAttempts = 0;
-    $('.numberattempts').html(numberAttempts);
-    cards.forEach(card => card.addEventListener('click', flipCard));
-     $('.game').show();
-    rehide ();
-    });
+	cards.forEach(card => { // ****Future Bug fix - For some reason when I add the function shuffle() here it stops gameplay but when I duplicate the shuffle code it operates as it should. Will investigate further to avoid code duplication.  
+		let randomPos = Math.floor(Math.random() * shufflenumber);
+		card.style.order = randomPos;
+		card.classList.remove('flip', 'match');
+		numberAttempts = 0;
+		$('.numberattempts').html(numberAttempts);
+		cards.forEach(card => card.addEventListener('click', flipCard));
+		$('.game').show();
+		rehide();
+	});
 }
 
 
 function win() { // checks if the amount of matches equal the shuffle number - if they do - the game has been completed - hides the cards and displays the results modal
-    matches = $('.match');
-    numbersmatched = matches.length;
-    if (numbersmatched == shufflenumber) {
-    setTimeout(function(){
-    $('.game').hide();}, 1200);
-    setTimeout(function(){
-    revealresult();}, 1500);
-}
-}
-
-function revealresult(){ //checks how many attempts player has made and shows the winning or losing modal
- if (numberAttempts <= (shufflenumber/2)+(4)) { 
-        $('.winner').show();
-        $("#win").removeClass("winner");
-       
-
-    } else { 
-            $('.loser').show();
-              $("#lose").removeClass("loser");
-        }
+	matches = $('.match');
+	numbersmatched = matches.length;
+	if (numbersmatched == shufflenumber) {
+		setTimeout(function () {
+			$('.game').hide();
+		}, 1200);
+		setTimeout(function () {
+			revealresult();
+		}, 1500);
+	}
 }
 
-function rehide () { // rehides the modal after a restard
-    $("#win").addClass("winner");
-    $('.winner').hide();
-    $("#lose").addClass("loser");
-    $('.loser').hide();
+function revealresult() { //checks how many attempts player has made and shows the winning or losing modal
+	if (numberAttempts <= (shufflenumber / 2) + (4)) {
+		$('.winner').show();
+		$("#win").removeClass("winner");
+
+
+	} else {
+		$('.loser').show();
+		$("#lose").removeClass("loser");
+	}
 }
 
+function rehide() { // rehides the modal after a restard
+	$("#win").addClass("winner");
+	$('.winner').hide();
+	$("#lose").addClass("loser");
+	$('.loser').hide();
+}
